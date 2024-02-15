af9d33cccda52b4f301dd31aeeb7ea600022f00e
FrankPolak <160171754+FrankPolak@users.noreply.github.com>
2024-02-15 20:54:08 +0100

    Multiple Linear Regression

---
+++ multiple_linear_regression.py
@@ -0,0 +1,29 @@
+# 1. Import Libraries and Dataset
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('heart.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# 2. Split Data into Training Set and Test Set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# 3. Train the Simple Linear Regression Model
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)  # predictor
+comparison = np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1)
+
+# 4. Evaluate the Model
+correct = 0
+for pred, test in comparison:
+    if pred < 0.5 and test == 0 or pred >= 0.5 and test == 1:
+        correct += 1
+
+accuracy = correct / len(comparison)
+print(accuracy) # 0.8217821782178217
